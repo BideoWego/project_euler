973122e4a9e5b815b8b0d8e7e22ae5785643f2e7
Bideo Wego <bideowego@gmail.com>
2018-01-29 18:42:07 -0500

    Remove tabs

--- 1_multiples_of_3_and_5/c/main.c
+++ 1_multiples_of_3_and_5/c/main.c
@@ -2,19 +2,19 @@
 
 int main(int argc, const char * argv[]) {
   int foo = 1;
-	int low = 0;
-	int high = 1000;
-	int sum = 0;
+  int low = 0;
+  int high = 1000;
+  int sum = 0;
 
-	while (low < high) {
-		int multipleOf3 = (low % 3 == 0);
-		int multipleOf5 = (low % 5 == 0);
-		if (multipleOf3 || multipleOf5) {
-			sum += low;
-		}
-		low++;
-	}
-	printf("Sum of all multiples of 3 and 5 between 0 and 1000 is: %d \n", sum);
+  while (low < high) {
+    int multipleOf3 = (low % 3 == 0);
+    int multipleOf5 = (low % 5 == 0);
+    if (multipleOf3 || multipleOf5) {
+      sum += low;
+    }
+    low++;
+  }
+  printf("Sum of all multiples of 3 and 5 between 0 and 1000 is: %d \n", sum);
 
-	return 0;
+  return 0;
 }
