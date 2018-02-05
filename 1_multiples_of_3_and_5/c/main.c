49265c15f384c8b658bcd6bcf8d77e90ea75a0ff
Bideo Wego <bideowego@gmail.com>
2018-02-05 02:26:31 -0500

    Update c answer problem 1

--- 1_multiples_of_3_and_5/c/main.c
+++ 1_multiples_of_3_and_5/c/main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-  int foo = 1;
-  int low = 0;
-  int high = 1000;
   int sum = 0;
 
-  while (low < high) {
-    int multipleOf3 = (low % 3 == 0);
-    int multipleOf5 = (low % 5 == 0);
-    if (multipleOf3 || multipleOf5) {
-      sum += low;
+  for (int i = 0; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
     }
-    low++;
   }
+
   printf("Sum of all multiples of 3 and 5 between 0 and 1000 is: %d \n", sum);
 
   return 0;
